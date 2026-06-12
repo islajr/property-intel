89979601bbd0de49e7c3a19415683028e2cb1f18
Isla <islarcherjr@icloud.com>
2026-06-12 13:18:21 +0100

    feat: (api) add market cache invalidation upon snapshot refresh

--- api/src/main/java/io/propertyintel/api/market/service/SnapshotScheduler.java
+++ api/src/main/java/io/propertyintel/api/market/service/SnapshotScheduler.java
@@ -1,8 +1,11 @@
 package io.propertyintel.api.market.service;
 
+import io.propertyintel.api.global.caching.CacheNames;
 import io.propertyintel.api.market.repository.MarketRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.dao.InvalidDataAccessResourceUsageException;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -15,7 +18,12 @@ public class SnapshotScheduler {
     private final MarketRepository marketRepository;
 
     @Scheduled(cron = "0 0 2 * * MON", zone = "GMT+1")    // Every monday at 02:00 GMT + 1
-    private void refreshMarketSnapshot() {
+    @Caching(evict = {
+            @CacheEvict(value = CacheNames.MARKET_DETAILS, allEntries = true),
+            @CacheEvict(value = CacheNames.MARKET_TRENDS, allEntries = true),
+            @CacheEvict(value = CacheNames.MARKET_NEIGHBOURHOODS, allEntries = true)
+    })
+    public void refreshMarketSnapshot() {
         log.info("-----Starting market snapshot refresh-----");
 
         long startTime = System.currentTimeMillis();
