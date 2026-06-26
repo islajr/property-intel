6b9f4870d6f4a98cb28961536d2faedeba8efe00
Isla <islarcherjr@icloud.com>
2026-06-26 11:25:43 +0100

    feat: (client) add page transition and card appearance animations

--- client/src/components/layout/PageShell.tsx
+++ client/src/components/layout/PageShell.tsx
@@ -1,13 +1,17 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Navigation from './Navigation';
 import Footer from './Footer';
 
 export default function PageShell() {
+  const location = useLocation();
+
   return (
     <div className="app-shell">
       <Navigation />
       <main className="main-content">
-        <Outlet />
+        <div key={location.pathname} className="page-transition-wrapper">
+          <Outlet />
+        </div>
       </main>
       <Footer />
     </div>
